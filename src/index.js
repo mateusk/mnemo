0bb20127fa8d61c044408cccf44e99a7acf1e89b
Mateus Knelsen <hello@mateus.works>
2020-12-10 00:15:37 +0100

    Add index.js with preliminary classes

---
+++ src/index.js
@@ -0,0 +1,25 @@
+class User {
+    constructor(name, age){
+        this.name = name,
+        this.age = age,
+        this.memories = []
+    }
+    /**
+     * @param {Memory} memory The memory to be added to the user's memory array.
+     */
+    addMemory(memory){
+        this.memories.push(memory);
+    }
+}
+
+class Memory {
+    /**
+     * @typedef {Object} Memory
+     * @param {string} text The memory text content.
+     * @param {string} place The place that relates to the memory.
+     */
+    constructor(text, place){
+        this.text = text
+        this.place = place
+    }
+}
