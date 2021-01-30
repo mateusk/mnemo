7fc80f75adc7cf1ea8f1a4db62c5023a395ff86a
Mateus Knelsen <hello@mateus.works>
2021-01-30 18:16:41 +0100

    Move dummy data from route files to 'dummydata.js' in /Models

--- backend/src/routes/mnemo.js
+++ backend/src/routes/mnemo.js
@@ -1,30 +1,7 @@
 const express = require('express')
-const Place = require('../models/place')
-const Memory = require('../models/memory')
+const { memories } = require('../models/dummydata')
 
 const router = express.Router()
-
-const places = [
-  new Place('Berlin', { latitude: 1, longitude: 1 }),
-  new Place('Berlin Hauptbahnhof', { latitude: 1, longitude: 1 }),
-  new Place('Hasenheide Park', { latitude: 1, longitude: 1 }),
-]
-
-const memories = [
-  new Memory('An unforgetable day', "This day was awesome, I'll never forget it", new Date(1995, 11, 17), places[0]),
-  new Memory(
-    'I met my wife here 20 years ago',
-    'My wife and I first met here 20 years ago.',
-    new Date(2003, 1, 31),
-    places[1]
-  ),
-  new Memory(
-    'Saw an ET here',
-    'It came as a great fireball from the sky and landed on the hill close to Columbiadamm',
-    new Date(1991, 7, 11),
-    places[2]
-  ),
-]
 
 /* GET query for specific memory title */
 router.get('/', (req, res) => {
