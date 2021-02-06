c0bb69c1bde44230e9dc26caac6e3c3851f0a513
Mateus Knelsen <mateus.knelsen@vw-fc.com>
2021-02-06 16:30:00 +0100

    WIP

--- backend/src/routes/mnemo.js
+++ backend/src/routes/mnemo.js
@@ -4,10 +4,9 @@ const { memories } = require('../models/dummydata')
 const router = express.Router()
 
 /* GET query for specific memory title */
-router.get('/', (req, res) => {
-  let result = []
-  if (req.query.title) {
-    result = memories.filter(memory => memory.title === req.query.title)
+router.get('/:title', (req, res) => {
+  let result = memories.filter(memory => memory.title === req.params.title)
+  if (result) {
     result = result.map(({ title, text, date, place: { name } }) => ({
       title,
       text,
@@ -15,7 +14,6 @@ router.get('/', (req, res) => {
       place: name,
     }))
   }
-  // res.send(result)
   res.render('mnemo', { result })
 })
 
