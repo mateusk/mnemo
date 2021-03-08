a7ca3ad1a51ae726f7a5587c94a9671c74c1d6af
Mateus Knelsen <sapientia_@hotmail.com>
2021-03-08 01:22:48 +0100

    Add port env. variable to backend database-connection.js

--- backend/src/database-connection.js
+++ backend/src/database-connection.js
@@ -4,11 +4,12 @@ const dbUsername = process.env.DB_USERNAME
 const dbPassword = process.env.DB_PASSWORD
 const dbName = process.env.DB_NAME
 const dbHost = process.env.DB_HOST
+const dbPort = process.env.DB_PORT
 const dbProtocol = process.env.DB_PROT
 const dbParams = process.env.DB_PARAMS
 
-let dbConnectionString = `${dbProtocol}://${dbHost}/${dbName}`
-if (dbUsername !== '')
+let dbConnectionString = `${dbProtocol}://${dbHost}:${dbPort}/${dbName}`
+if (dbUsername !== undefined)
   dbConnectionString = `${dbProtocol}://${dbUsername}:${dbPassword}@${dbHost}/${dbName}?${dbParams}`
 
 mongoose.set('debug', true)
