c4f87590690a6f1fb2265c4001d0a76a35e5a43f
Mateus Knelsen <sapientia_@hotmail.com>
2021-03-13 17:51:39 +0100

    Create frontend project

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  .home
+    HelloWorld
+    div(v-for="user in users") {{ user.username }} has {{ user.memories.length }} memories
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      users: [],
+    }
+  },
+  async created() {
+    const usersRequest = await axios.get('/api/users')
+
+    this.users = usersRequest.data
+  },
+}
+</script>
