8a8e0af0dec336712ef6c66242d35cf6f57daa61
Mateus Knelsen <sapientia_@hotmail.com>
2021-03-13 23:06:44 +0100

    Update frontend home view

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,25 +1,14 @@
-<template lang="pug">
-  .home
-    HelloWorld
-    div(v-for="user in users") {{ user.username }} has {{ user.memories.length }} memories
-</template>
-
 <script>
-// @ is an alias to /src
-import axios from 'axios'
-
 export default {
   name: 'Home',
   components: {},
   data() {
-    return {
-      users: [],
-    }
-  },
-  async created() {
-    const usersRequest = await axios.get('/api/users')
-
-    this.users = usersRequest.data
+    return {}
   },
 }
 </script>
+
+<template lang="pug">
+  .home
+    p Welcome to Mnemo!
+</template>
