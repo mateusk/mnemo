2c0d52f266a322458956c9b854ae978f7223931f
Mateus Knelsen <sapientia_@hotmail.com>
2021-03-15 23:22:22 +0100

    Rename "app.vue" and "home.vue" frontend files

---
+++ frontend/src/app.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+  #app
+    h1 Mnemo
+    #nav
+      router-link(to="/") Home
+      router-link(to="/users") Users
+      router-link(to="/memories") Memories
+      router-link(to="/places") Places
+    router-view
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  padding: 50px;
+  color: #2c3e50;
+}
+
+#nav {
+  padding-bottom: 50px;
+  a {
+    padding-right: 30px;
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
